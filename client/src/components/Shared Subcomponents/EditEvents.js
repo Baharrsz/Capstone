6d92b5c0f0ef6f3763f5670cdda201dceb6e433e
Bahar Raiszadeh <Bahar@Bahars-MacBook-Pro.local>
2019-12-05 17:10:41 -0800

    Changed EditEvents: It shows descendant events. The code is reeeeally difficult to read though

--- client/src/components/Shared Subcomponents/EditEvents.js
+++ client/src/components/Shared Subcomponents/EditEvents.js
@@ -7,6 +7,8 @@ export default class EditEvents extends Component {
   }
   render() {
     const { ancestors, descendants, main } = this.props.savedPlans;
+
+    //Creating html for showing ancestor events
     const ancestorsEvents = Object.keys(ancestors).map(ancestor => {
       if (ancestors[ancestor]) {
         const events = Object.values(ancestors[ancestor].events);
@@ -37,13 +39,64 @@ export default class EditEvents extends Component {
       }
     });
 
+    //Creating html for showing descendant events
+    const descendantsEvents = Object.keys(descendants).map(
+      descendantArrayName => {
+        if (descendants[descendantArrayName]) {
+          ////
+          const descendantArray = descendants[descendantArrayName];
+          return (
+            <div className="planning__section-transferred">
+              <h4 className="planning__section-transferred__collection-title">{`${descendantArrayName} Events`}</h4>
+              <div className="planning__section-transferred__collection">
+                {descendantArray.map(descendant => {
+                  const events = Object.values(descendant.events);
+                  return (
+                    <div>
+                      <h5 className="planning__section-transferred__collection-title">
+                        {descendant.id}
+                      </h5>
+                      <div className="planning__section-transferred__collection-items">
+                        {events.map(event => {
+                          return Object.keys(event).map(key => {
+                            return (
+                              <div
+                                className="planning__section-transferred-events"
+                                key={uuid()}
+                              >
+                                <label className="planning__section-transferred-events-title">
+                                  {key}
+                                </label>
+                                <div className="wplanning__section-transferred-events-value">
+                                  {event[key]}
+                                </div>
+                              </div>
+                            );
+                          });
+                        })}
+                      </div>
+                      ;
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+
+          ////
+        }
+      }
+    );
+
     return (
       <div className="planning__events">
         <h3 className="planning__section-title">Events</h3>
         <div className="planning-events-ancestors planning-section-ancestors">
           {ancestorsEvents}
         </div>
-        <div className="planning-events-descendants planning-section-descendants"></div>
+        <div className="planning-events-descendants planning-section-descendants">
+          {descendantsEvents}
+        </div>
       </div>
     );
   }
